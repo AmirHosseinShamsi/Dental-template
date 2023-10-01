2ed7b89f462c68bfa9dc2dfb22880cc1c868d7da
amirhosseinShamsi <shamsiamir2000@gmail.com>
2023-10-02 00:32:17 +0330

    bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import BrainIcon from '../public/Icon/brain.svg';
 export default function App() {
 
   return (
       <>
-          <i className="fa-solid fa-envelope" style={{color:'green'}}></i>
+          <button className="btn btn-danger">test</button>
       </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import '../public/fontawesome/js/all.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
