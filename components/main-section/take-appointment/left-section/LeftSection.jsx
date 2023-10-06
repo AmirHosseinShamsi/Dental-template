8f34b5a1a1ed183d236fee210456f3020f024260
amirhosseinShamsi <shamsiamir2000@gmail.com>
2023-10-07 00:18:11 +0330

    final left side

--- components/main-section/take-appointment/left-section/LeftSection.jsx
+++ components/main-section/take-appointment/left-section/LeftSection.jsx
@@ -14,30 +14,30 @@ const LeftSection = () => {
         id: 'nameInput',
     },
         {
-        label: 'email address',
-        placeholder: 'email',
-        type: 'text',
-        align: '',
-        id: 'emailInput',
-    },{
-        label: 'phone number',
-        placeholder: 'phone number',
-        type: 'text',
-        align: '',
-        id: 'phoneInput',
-    },{
-        label: 'appointment date',
-        placeholder: 'appointment date',
-        type: 'text',
-        align: '',
-        id: 'appointmentInput',
-    },{
-        label: 'appointment time',
-        placeholder: 'appointment time',
-        type: 'text',
-        align: '',
-        id: 'timeInput',
-    }]
+            label: 'email address',
+            placeholder: 'email',
+            type: 'text',
+            align: '',
+            id: 'emailInput',
+        }, {
+            label: 'phone number',
+            placeholder: 'phone number',
+            type: 'text',
+            align: '',
+            id: 'phoneInput',
+        }, {
+            label: 'appointment date',
+            placeholder: 'appointment date',
+            type: 'text',
+            align: '',
+            id: 'appointmentInput',
+        }, {
+            label: 'appointment time',
+            placeholder: 'appointment time',
+            type: 'text',
+            align: '',
+            id: 'timeInput',
+        }]
 
     const selectData = {
         label: 'choose a doctor',
@@ -48,7 +48,7 @@ const LeftSection = () => {
         }, {
             value: 'two',
             text: 'two',
-        },{
+        }, {
             value: 'two',
             text: 'two',
         }
@@ -56,10 +56,10 @@ const LeftSection = () => {
     }
     return (
         <div className={`col-lg-6 col-12 ${classes.leftSectionBackground}`}>
-            <div className="row m-0 justify-content-lg-end justify-content-center">
-                <Heading/>
+            <div className="row m-0 justify-content-center">
+                <Heading text="we are here for you" heading="make an appointment"/>
                 <div className="col-10 ps-0">
-                    <form action="#" className="row m-0">
+                    <div className="row m-0">
                         {inputData.map((item, index) => {
                             return <Input data={{...item}}/>
                         })}
@@ -71,7 +71,7 @@ const LeftSection = () => {
                                 massages
                             </button>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
