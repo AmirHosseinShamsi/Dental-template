79c8d3b0c58f4945deaea0d9995f1fb2cc2cc2b0
amirhosseinShamsi <shamsiamir2000@gmail.com>
2023-10-06 23:57:18 +0330

    left section 90% done

--- components/main-section/take-appointment/TakeAppointment.jsx
+++ components/main-section/take-appointment/TakeAppointment.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import LeftSection from "components/main/take-appointment/left-section/LeftSection.jsx";
-import RightSection from "components/main/take-appointment/right-section/RightSection.jsx";
+import LeftSection from "../../../components/main-section/take-appointment/left-section/LeftSection.jsx";
+import RightSection from "../../../components/main-section/take-appointment/right-section/RightSection.jsx";
 
 const TakeAppointment = () => {
     return (
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "../components/Header/Header.jsx";
+import TakeAppointment from "../components/main-section/take-appointment/TakeAppointment.jsx";
 
 export default function App() {
 
   return (
       <>
           <Header/>
+          <main>
+              <TakeAppointment/>
+          </main>
       </>
   )
 }
