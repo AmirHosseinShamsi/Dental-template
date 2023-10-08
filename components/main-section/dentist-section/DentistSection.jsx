0338b1905111095083c02c22fb753138d563894f
amirhosseinShamsi <shamsiamir2000@gmail.com>
2023-10-08 19:19:47 +0330

    doctor section 70%

--- components/main-section/dentist-section/DentistSection.jsx
+++ components/main-section/dentist-section/DentistSection.jsx
@@ -1,6 +1,49 @@
 import React from 'react';
 import classes from './DentistSection.module.css';
+import DoctorCards from "../../../components/main-section/dentist-section/doctor-cards/DoctorCards.jsx";
+import doctorImage from '../../../public/images/card-image1.webp';
+
 const DentistSection = () => {
+    const data = [{
+        image: doctorImage,
+        fullName: 'Dr. Alicia Henderson',
+        position: 'dentist',
+        socialMedia: [{
+            icon: 'fa-brands fa-twitter',
+        }, {
+            icon: 'fa-brands fa-facebook',
+        }, {
+            icon: 'fa-brands fa-instagram',
+        }, {
+            icon: 'fa-brands fa-telegram'
+        }]
+    },{
+        image: doctorImage,
+        fullName: 'Dr. Alicia Henderson',
+        position: 'dentist',
+        socialMedia: [{
+            icon: 'fa-brands fa-twitter',
+        }, {
+            icon: 'fa-brands fa-facebook',
+        }, {
+            icon: 'fa-brands fa-instagram',
+        }, {
+            icon: 'fa-brands fa-telegram'
+        }]
+    },{
+        image: doctorImage,
+        fullName: 'Dr. Alicia Henderson',
+        position: 'dentist',
+        socialMedia: [{
+            icon: 'fa-brands fa-twitter',
+        }, {
+            icon: 'fa-brands fa-facebook',
+        }, {
+            icon: 'fa-brands fa-instagram',
+        }, {
+            icon: 'fa-brands fa-telegram'
+        }]
+    },]
     return (
         <section className={classes.doctorSection}>
             <div className="container-md p-0">
@@ -8,117 +51,9 @@ const DentistSection = () => {
                     <span className={`col-12 text-uppercase text-center mb-2 ${classes.shortText}`}>Our doctors</span>
                     <h5 className={`col-12 text-capitalize text-center ${classes.headingText}`}>Qualified Dentist</h5>
                     <div className="row m-0 gx-3">
-                        {/*<div className="col-lg-3 col-sm-6 col-12 cards-self">
-                            <div className="card card-body text-center row m-0 border-0">
-                                <div className="img-sec-for-ca">
-                                    <img src="img/card-image4.webp" className="p-0 img-for-ca1" alt="card-image"/>
-                                </div>
-                                <h5 className="card-title text-capitalize mb-3 mt-4 card-title-h5 col-12">Dr. Alicia Henderson</h5>
-                                <h6 className="card-subtitle mb-2 text-muted text-uppercase card-subtitle-h6 col-12">dentist</h6>
-                                <div className="icon-s-w col-12 mt-3">
-
-
-                                    <div className="row m-0 justify-content-center justify-content-sm-between">
-                                        <div className="icon-first btn mx-2 mx-sm-0"><i className="fa-brands fa-twitter"></i></div>
-                                        <div className="icon-first btn mx-2 mx-sm-0"><i className="fa-brands fa-facebook"></i></div>
-                                        <div className="icon-first btn mx-2 mx-sm-0"><i className="fa-brands fa-instagram"></i>
-                                        </div>
-                                        <div className="icon-first btn ms-2 ms-sm-0"><i className="fa-brands fa-telegram"></i></div>
-
-
-                                    </div>
-
-                                </div>
-                                <p className="card-text col-12 mt-3 mb-4">I am an ambitious workaholic, but apart from that, pretty
-                                    simple
-                                    person.</p>
-
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-sm-6 col-12 cards-self">
-
-                            <div className="card card-body text-center row m-0 border-0">
-                                <div className="img-sec-for-ca">
-                                    <img src="img/card-image4.webp" className="p-0 img-for-ca1"/>
-                                </div>
-                                <h5 className="card-title text-capitalize mb-3 mt-4 card-title-h5 col-12">Dr. Alicia Henderson</h5>
-                                <h6 className="card-subtitle mb-2 text-muted text-uppercase card-subtitle-h6 col-12">dentist</h6>
-                                <div className="icon-s-w col-12 mt-3">
-
-
-                                    <div className="row m-0 justify-content-center justify-content-sm-between">
-                                        <div className="icon-first btn mx-2 mx-sm-0"><i className="fa-brands fa-twitter"></i></div>
-                                        <div className="icon-first btn mx-2 mx-sm-0"><i className="fa-brands fa-facebook"></i></div>
-                                        <div className="icon-first btn mx-2 mx-sm-0"><i className="fa-brands fa-instagram"></i>
-                                        </div>
-                                        <div className="icon-first btn ms-2 ms-sm-0"><i className="fa-brands fa-telegram"></i></div>
-
-
-                                    </div>
-
-                                </div>
-                                <p className="card-text col-12 mt-3 mb-4">I am an ambitious workaholic, but apart from that, pretty
-                                    simple
-                                    person.</p>
-
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-sm-6 col-12 cards-self">
-
-                            <div className="card card-body text-center row m-0 border-0">
-                                <div className="img-sec-for-ca">
-                                    <img src="img/card-image4.webp" className="p-0 img-for-ca1"/>
-                                </div>
-                                <h5 className="card-title text-capitalize mb-3 mt-4 card-title-h5 col-12">Dr. Alicia Henderson</h5>
-                                <h6 className="card-subtitle mb-2 text-muted text-uppercase card-subtitle-h6 col-12">dentist</h6>
-                                <div className="icon-s-w col-12 mt-3">
-
-
-                                    <div className="row m-0 justify-content-center justify-content-sm-between">
-                                        <div className="icon-first btn mx-2 mx-sm-0"><i className="fa-brands fa-twitter"></i></div>
-                                        <div className="icon-first btn mx-2 mx-sm-0"><i className="fa-brands fa-facebook"></i></div>
-                                        <div className="icon-first btn mx-2 mx-sm-0"><i className="fa-brands fa-instagram"></i>
-                                        </div>
-                                        <div className="icon-first btn ms-2 ms-sm-0"><i className="fa-brands fa-telegram"></i></div>
-
-
-                                    </div>
-
-                                </div>
-                                <p className="card-text col-12 mt-3 mb-4">I am an ambitious workaholic, but apart from that, pretty
-                                    simple
-                                    person.</p>
-
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-sm-6 col-12 cards-self">
-
-                            <div className="card card-body text-center row m-0 border-0">
-                                <div className="img-sec-for-ca">
-                                    <img src="img/card-image4.webp" className="p-0 img-for-ca1"/>
-                                </div>
-                                <h5 className="card-title text-capitalize mb-3 mt-4 card-title-h5 col-12">Dr. Alicia Henderson</h5>
-                                <h6 className="card-subtitle mb-2 text-muted text-uppercase card-subtitle-h6 col-12">dentist</h6>
-                                <div className="icon-s-w col-12 mt-3">
-
-
-                                    <div className="row m-0 justify-content-center justify-content-sm-between">
-                                        <div className="icon-first btn mx-2 mx-sm-0"><i className="fa-brands fa-twitter"></i></div>
-                                        <div className="icon-first btn mx-2 mx-sm-0"><i className="fa-brands fa-facebook"></i></div>
-                                        <div className="icon-first btn mx-2 mx-sm-0"><i class="fa-brands fa-instagram"></i>
-                                        </div>
-                                        <div class="icon-first btn ms-2 ms-sm-0"><i class="fa-brands fa-telegram"></i></div>
-
-
-                                    </div>
-
-                                </div>
-                                <p class="card-text col-12 mt-3 mb-4">I am an ambitious workaholic, but apart from that, pretty
-                                    simple
-                                    person.</p>
-
-                            </div>
-                        </div>*/}
+                        {data.map((item, index) => {
+                            return <DoctorCards data={{...item}}/>
+                        })}
                     </div>
                 </div>
             </div>
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header/Header.jsx";
 import TakeAppointment from "../components/main-section/take-appointment/TakeAppointment.jsx";
 import Services from "../components/main-section/services/Services.jsx";
 import IntroduceSection from "../components/main-section/introduce-section/IntroduceSection.jsx";
+import DentistSection from "../components/main-section/dentist-section/DentistSection.jsx";
 
 export default function App() {
 
@@ -12,6 +13,7 @@ export default function App() {
               <TakeAppointment/>
               <Services/>
               <IntroduceSection/>
+              <DentistSection/>
           </main>
       </>
   )
