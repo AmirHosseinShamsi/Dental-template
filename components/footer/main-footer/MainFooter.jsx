1e61e895caf8330f1c22d76eaa0150bcc84e5b79
amirhosseinShamsi <shamsiamir2000@gmail.com>
2023-11-12 23:44:28 +0330

    blog posts done

--- components/footer/main-footer/MainFooter.jsx
+++ components/footer/main-footer/MainFooter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './MainFooter.module.css';
 import TextLogo from "../../../components/footer/main-footer/text-logo/TextLogo.jsx";
 import List from "../../../components/footer/main-footer/list/List.jsx";
+import BlogPost from "../../../components/footer/main-footer/blog-posts/BlogPost.jsx";
 
 const MainFooter = () => {
     const servicesListData = [{
@@ -42,88 +43,26 @@ const MainFooter = () => {
                     <div className="col-lg-2 mt-lg-0 col-md-6 mt-5">
                         <List title="Quick Links" data={QuickListData}/>
                     </div>
+                   {/* <div className="col-lg-3 mt-lg-0 col-md-6 mt-5">
+                        <div className="bg-transparent">
+                            <h6 className="text-white mb-4 text-capitalize"
+                                style="font-size: 18px;font-family: 'Roboto-Medium';">recent posts
+                            </h6>
+                            <BlogPost/>
+                            <BlogPost/>
+                            <BlogPost/>
+                        </div>
+                    </div>*/}
                     {/*
                     <div className="col-lg-3 mt-lg-0 col-md-6 mt-5">
-                        <div className="recent-post-footer bg-transparent">
+                        <div className="bg-transparent">
                             <h6 className="text-white mb-4 text-capitalize"
-                                style="font-size: 18px;font-family: 'Roboto-Medium';">recent posts</h6>
-                            <div className="item-1-f row m-0 bg-transparent">
-                                <img src="img/card-image2.webp" className="p-0 rounded col-2"
-                                     style="width: 50px; height: 50px;background-size:cover;background-position: center;object-fit: cover;">
-                                    <div className="text-date col-10">
-                                        <ul className="list-group d-flex flex-row">
-                                            <li className="list-group-item bg-transparent border-0  px-0"
-                                                style="font-size: 12px;color:white ;">
-                                                <span className="fa fa-solid fa-calendar me-1"></span>Jan. 18, 2021
-                                            </li>
-                                            <li className="list-group-item bg-transparent border-0  px-0 text-capitalize ms-2"
-                                                style="font-size: 12px;color:white;"><span
-                                                className="fa fa-solid fa-user me-1"></span>Admin
-                                            </li>
-                                        </ul>
-                                        <h3 className="text-capitalize"
-                                            style="font-size: 16px;color:white;font-family: 'Roboto-Medium';">
-                                            Creativity and Inspiration</h3>
-                                    </div>
-                            </div>
-                            <div className="item-1-f row m-0 bg-transparent mt-2">
-                                <img src="img/card-image2.webp" className="p-0 rounded col-2"
-                                     style="width: 50px; height: 50px;background-size:cover;background-position: center;object-fit: cover;">
-                                    <div className="text-date col-10">
-                                        <ul className="list-group d-flex flex-row">
-                                            <li className="list-group-item bg-transparent border-0  px-0"
-                                                style="font-size: 12px;color:white ;">
-                                                <span className="fa fa-solid fa-calendar me-1"></span>Jan. 18, 2021
-                                            </li>
-                                            <li className="list-group-item bg-transparent border-0  px-0 text-capitalize ms-2"
-                                                style="font-size: 12px;color:white;"><span
-                                                className="fa fa-solid fa-user me-1"></span>Admin
-                                            </li>
-                                        </ul>
-                                        <h3 className="text-capitalize"
-                                            style="font-size: 16px;color:white;font-family: 'Roboto-Medium';">
-                                            Creativity and Inspiration</h3>
-                                    </div>
-                            </div>
-                            <div className="item-1-f row m-0 bg-transparent mt-2">
-                                <img src="img/card-image2.webp" className="p-0 rounded col-2"
-                                     style="width: 50px; height: 50px;background-size:cover;background-position: center;object-fit: cover;">
-                                    <div className="text-date col-10">
-                                        <ul className="list-group d-flex flex-row">
-                                            <li className="list-group-item bg-transparent border-0  px-0"
-                                                style="font-size: 12px;color:white ;">
-                                                <span className="fa fa-solid fa-calendar me-1"></span>Jan. 18, 2021
-                                            </li>
-                                            <li className="list-group-item bg-transparent border-0  px-0 text-capitalize ms-2"
-                                                style="font-size: 12px;color:white;"><span
-                                                className="fa fa-solid fa-user me-1"></span>Admin
-                                            </li>
-                                        </ul>
-                                        <h3 className="text-capitalize"
-                                            style="font-size: 16px;color:white;font-family: 'Roboto-Medium';">
-                                            Creativity and Inspiration</h3>
-                                    </div>
-                            </div>
+                                style="font-size: 18px;font-family: 'Roboto-Medium';">recent posts
+                                </h6>
                         </div>
                     </div>
                     <div className="col-lg-3 mt-lg-0 col-md-6 mt-5">
-                        <div className="have-a-question-footer bg-transparent">
-                            <h3 className="text-capitalize text-white" style="font-size: 18px;font-family: 'Roboto-Medium';">
-                                Have a Questions?</h3>
-                            <p className="text-capitalize" style="font-size: 15px;color: white; font-family: 'Roboto-Regular';">
-                                <span className="fa-solid fa-map me-2"></span>
-                                203 Fake St. Mountain View, San Francisco, California, USA
-                            </p>
-                            <p class="text-capitalize" style="font-size: 15px;color: white; font-family: 'Roboto-Regular';">
-                                <span class="fa-solid fa-phone me-2"></span>
-                                +2 392 3929 210
 
-                            </p>
-                            <p class="text-capitalize" style="font-size: 15px;color: white; font-family: 'Roboto-Regular';">
-                                <span class="fa-solid fa-mail-bulk me-2"></span>
-                                info@yourdomain.com
-                            </p>
-                        </div>
                     </div>*/}
                 </div>
 
---
+++ components/footer/main-footer/blog-posts/BlogPost.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './BlogPost.module.css';
+import blogImage from '../../../../public/images/blog-image1.webp';
+
+const BlogPost = () => {
+    return (
+        <div className="row m-0 bg-transparent">
+            <img src={blogImage} className={`p-0 rounded col-2 ${classes.blogImage}`} alt="blog-image"/>
+            <div className="col-10">
+                <ul className="list-group d-flex flex-row">
+                    <li className={`list-group-item bg-transparent border-0 px-0 ${classes.listItems}`}>
+                        <span className="fa fa-solid fa-calendar me-1"></span>Jan. 18, 2021
+                    </li>
+                    <li className={`list-group-item bg-transparent border-0 px-0 text-capitalize ms-2 ${classes.listItems}`}><span
+                        className="fa fa-solid fa-user me-1"></span>Admin
+                    </li>
+                </ul>
+                <h3 className={`text-capitalize ${classes.textBlog}`}>Creativity and Inspiration</h3>
+            </div>
+        </div>
+    );
+};
+
+export default BlogPost;
