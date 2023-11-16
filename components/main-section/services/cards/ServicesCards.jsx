9507a137c08aa794184b2127522c4153478ff402
amirhosseinShamsi <shamsiamir2000@gmail.com>
2023-11-16 23:08:12 +0330

    refactor 2

--- components/main-section/services/cards/ServicesCards.jsx
+++ components/main-section/services/cards/ServicesCards.jsx
@@ -4,15 +4,15 @@ import classes from './ServicesCards.module.css';
 const ServicesCards = ({data}) => {
     return (
         <div className="col-12 col-md-6 col-lg-3">
-            <div className="row m-0 border bg-light py-4 px-1 justify-content-center border-0 rounded-1">
-                <div className={`text-center col-12 mb-4 ${classes.iconSection}`}>
-                    <span className={`${data.icon} ${classes.iconStyle}`}></span>
+                <div className={`border bg-light border-0 rounded-1 ${classes.cardServices}`}>
+                    <div className={`text-center col-12 mb-4 mx-auto ${classes.iconSection}`}>
+                        <span className={`${data.icon} ${classes.iconStyle}`}></span>
+                    </div>
+                    <div className="col-12 text-center">
+                        <h6 className={classes.cardHeading}>{data.heading}</h6>
+                        <p className={`text-muted ${classes.cardDescription}`}>{data.description}</p>
+                    </div>
                 </div>
-                <div className="col-12 text-center">
-                    <h6 className={classes.cardHeading}>{data.heading}</h6>
-                    <p className={`text-muted ${classes.cardDescription}`}>{data.description}</p>
-                </div>
-            </div>
         </div>
     );
 };
