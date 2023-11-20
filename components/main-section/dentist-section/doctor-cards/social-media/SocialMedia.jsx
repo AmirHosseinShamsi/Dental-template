c4d81a2ec05816bf6185d2a2f573fb5d3dec0ae2
amirhosseinShamsi <shamsiamir2000@gmail.com>
2023-11-20 17:17:28 +0330

    Updates

--- components/main-section/dentist-section/doctor-cards/social-media/SocialMedia.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './SocialMedia.module.css';
-const SocialMedia = ({data}) => {
-    return (
-        <div className={`btn mx-2 mx-sm-0 ${classes.socialMediaSection}`}><i className={`${data.icon} ${classes.socialMediaIcon}`}></i></div>
-    );
-};
-
-export default SocialMedia;
